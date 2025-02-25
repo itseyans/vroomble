8bc0954a375e257ea66ce111605a5c87c7fae1c2
Kimochei <kmpsobrepena@mymail.mapua.edu.ph>
2025-02-26 00:30:26 +0800

    ABOUT US ADDED
    Footer Adjusted

--- src/vcomp/Footer.js
+++ src/vcomp/Footer.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link"; // Import the Link component
 import styled from "styled-components";
 
 const FooterContainer = styled.footer`
@@ -15,27 +16,31 @@ const FooterItems = styled.div`
   justify-content: center;
 `;
 
-const FooterItem = styled.a`
-  color: gold;
-  text-decoration: none;
-  margin: 0 1rem;
-  padding: 0.5rem 1rem;
-  border-radius: 4px;
+const FooterItem = styled.div` // Changed from 'a' to 'div'
+    color: gold;
+    text-decoration: none;
+    margin: 0 1rem;
+    padding: 0.5rem 1rem;
+    border-radius: 4px;
 
-  &:hover {
-    background-color: rgba(255, 215, 0, 0.2);
-  }
+    &:hover {
+        background-color: rgba(255, 215, 0, 0.2);
+    }
 `;
 
 const Footer = () => {
-                              return (
-                                <FooterContainer>
-                                  <FooterItems>
-                                    <FooterItem href="#">Contact</FooterItem> {/* About in footer */}
-                                    <FooterItem href="#">About Us</FooterItem> {/* Contact in footer */}
-                                  </FooterItems>
-                                </FooterContainer>
-                              );
-                            };
+    return (
+        <FooterContainer>
+            <FooterItems>
+                <Link href="/about">
+                    <FooterItem>Contact</FooterItem> {/* No more <a> tag here */}
+                </Link>
+                <Link href="/about">
+                    <FooterItem>About Us</FooterItem>
+                </Link>
+            </FooterItems>
+        </FooterContainer>
+    );
+};
 
 export default Footer;
