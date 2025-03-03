b71a8964a9d72f3847ed02e105d735624e1f19e3
miggyiggy <juanmolina@mymail.mapua.edu.ph>
2025-03-04 03:02:17 +0800

    UserVehicleRegistration page

--- src/app/user_vehicle_registration/page.tsx
+++ src/app/user_vehicle_registration/page.tsx
@@ -1,1 +1,73 @@
-a
+"use client";
+
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import BackgroundImage from '../../vcomp/BackgroundImage.js';
+import NavBar from '../../vcomp/NavBar.js';
+import RegisterVehicle from "../../vcomp/RegisterVehicle.js";
+import Footer from "../../vcomp/Footer.js";
+
+const AppContainer = styled.div` 
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #131415;
+  color: gold;
+`;
+
+const NavBarContainer = styled.div`
+  /* Add any specific styling for the NavBar container if needed */
+`;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column; 
+  justify-content: center; // Center the button vertically
+  align-items: center;
+  flex-grow: 1; // Allow this container to take up available space
+  background-color: #131415;
+  color: gold;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #242424;
+  color: gold;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #333;
+  }
+`;
+const VehicleRegistrationPage = () => {
+    const [showForm, setShowForm] = useState(false);
+  
+    const handleButtonClick = () => {
+      setShowForm(true);
+    };
+  
+    return (
+      <AppContainer> 
+        <NavBarContainer> 
+          <NavBar /> 
+        </NavBarContainer>
+        <PageContainer> 
+          {showForm ? (
+            <RegisterVehicle /> 
+          ) : (
+            <Button onClick={handleButtonClick}>Register a Vehicle</Button>
+          )}
+        </PageContainer>
+        <Footer />
+      </AppContainer>
+    );
+  };
+  
+  export default VehicleRegistrationPage;
+
+
