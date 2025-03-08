ac7bfbf649ff4eddc67be8afc2569c98d3905563
Timeless <92083422+Krashedd@users.noreply.github.com>
2025-03-08 21:07:13 +0800

    Welcome Nav Bar Complete

--- src/vcomp/WelcomeNavBar.js
+++ src/vcomp/WelcomeNavBar.js
@@ -6,107 +6,106 @@ import RegistrationForm from "./RegistrationForm.js";
 import LoginForm from "./LoginForm.js";
 
 const NavBarContainer = styled.nav`
-  margin-bottom: 50px;
   background-color: black;
   color: gold;
   display: flex;
   align-items: center;
-  padding: 1rem;
-  justify-content: center;
-`;
-
-const NavItems = styled.div`
-  display: flex;
-  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 4rem;
+  position: relative;
+  height: 8rem;
 `;
 
 const LogoContainer = styled.div`
   display: flex;
   align-items: center;
+  font-family: 'Shrikhand', cursive;
+  font-size: 3.5rem;
+  color: gold;
 `;
 
-const Logo = styled.span`
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin-right: 2rem;
-`;
-
-const EmblemContainer = styled.div`
-  background-color: #e0e0e0;
-  border-radius: 50%;
-  padding: 0.5rem;
+const EmblemBackground = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #d9d9d9;
+  width: 15rem;
+  height: 8rem;
+  clip-path: polygon(12% 100%, 88% 100%, 100% 0, 0 0);
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 3rem;
-  height: 3rem;
+  z-index: 2;
+`;
+
+const EmblemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 7rem;
+  height: 7rem;
 `;
 
 const Emblem = styled.img`
-  max-width: 100%;
+  width: 100%;
   height: auto;
 `;
 
-const RegisterButton = styled.button`
-  background-color: gold;
-  color: black;
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-left: 50px;
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 15px;
 `;
 
-const LoginButton = styled.button`
-  background-color: gold;
+const NavButton = styled.button`
+  background-color: white;
   color: black;
-  padding: 0.5rem 1rem;
+  font-weight: bold;
+  padding: 0.8rem 2rem;
+  font-size: 1.2rem;
   border: none;
-  border-radius: 4px;
+  border-radius: 30px;
   cursor: pointer;
-  margin-left: 1100px;
+  transition: background 0.3s;
+
+  &:hover {
+    background-color: gold;
+  }
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem; /* Added margin to push forms lower */
 `;
 
 const WelcomeNavBar = () => {
-  const [showReg, setShowReg] = useState(false);
-  const [showLog, setShowLog] = useState(false);
+  const [activeForm, setActiveForm] = useState(null);
 
-  const handleLogin = () => {
-    setShowLog(true);
-    setShowReg(false); // Hide registration form
-  };
-
-  const handleRegister = () => {
-    setShowReg(true);
-    setShowLog(false); // Hide login form
+  const toggleForm = (formType) => {
+    setActiveForm((prevForm) => (prevForm === formType ? null : formType));
   };
 
   return (
     <>
       <NavBarContainer>
-        <NavItems>
-          <LogoContainer>
-            <Logo>VROOMBLE</Logo>
-            <EmblemContainer>
-              <Emblem src="/images/emblem.png" alt="Emblem" />
-            </EmblemContainer>
-          </LogoContainer>
-          <>
-            <LoginButton onClick={handleLogin}>Login</LoginButton>
-          </>
-          <RegisterButton onClick={handleRegister}>Register</RegisterButton>
-        </NavItems>
+        <LogoContainer>VROOMBLE</LogoContainer>
+        <EmblemBackground>
+          <EmblemContainer>
+            <Emblem src="/LOGO.png" alt="Vroomble Logo" />
+          </EmblemContainer>
+        </EmblemBackground>
+        <ButtonContainer>
+          <NavButton onClick={() => toggleForm("login")}>Sign In</NavButton>
+          <NavButton onClick={() => toggleForm("register")}>Register</NavButton>
+        </ButtonContainer>
       </NavBarContainer>
 
-      {/* The forms after button click */}
-
-      <center>
-        {showReg && <RegistrationForm />}
-      </center>
-
-      <center>
-        {showLog && <LoginForm />}
-      </center>
+      {/* Show Forms on Click */}
+      <FormContainer>
+        {activeForm === "register" && <RegistrationForm />}
+        {activeForm === "login" && <LoginForm />}
+      </FormContainer>
     </>
   );
 };
